80a256101de9289a8b2f7630a03a470e8142909c
Hugo-Latreille <hugo.latreille@gmail.com>
2022-12-29 17:25:13 +0100

    :wrench: O2switch config

--- app/db/connect.js
+++ app/db/connect.js
@@ -1,19 +1,19 @@
-const Pool = require("pg-pool");
+// const Pool = require("pg-pool");
 
-const pool = new Pool({
-	connectionString:
-		"postgres://tjfwqego:bHCjuaUdNMKKWcF03C1LEqDp7wmpN3sQ@mel.db.elephantsql.com/tjfwqego",
-	ssl: {
-		rejectUnauthorized: false,
-	},
-});
+// const pool = new Pool({
+// 	connectionString:
+// 		"postgres://tjfwqego:bHCjuaUdNMKKWcF03C1LEqDp7wmpN3sQ@mel.db.elephantsql.com/tjfwqego",
+// 	ssl: {
+// 		rejectUnauthorized: false,
+// 	},
+// });
 
-pool.connect((err) => {
-	if (err) {
-		console.error("erreur de connection", err.stack);
-	} else {
-		console.error("connection OK");
-	}
-});
+// pool.connect((err) => {
+// 	if (err) {
+// 		console.error("erreur de connection", err.stack);
+// 	} else {
+// 		console.error("connection OK");
+// 	}
+// });
 
-module.exports = pool;
+// module.exports = pool;
