45a92dd22116c2d96bebe8d6b7a2abc27724945d
Hugo-Latreille <hugo.latreille@gmail.com>
2022-12-29 15:35:25 +0100

    :wrench: O2switch config

--- app/db/connect.js
+++ app/db/connect.js
@@ -3,9 +3,9 @@ const Pool = require("pg-pool");
 const pool = new Pool({
 	connectionString:
 		"postgres://develott:OukF0DrRC7zU@ep-black-cloud-602350.eu-central-1.aws.neon.tech/develott?sslmode=require&options=project%3Dep-black-cloud-602350",
-	ssl: {
-		rejectUnauthorized: false,
-	},
+	// ssl: {
+	// 	rejectUnauthorized: false,
+	// },
 });
 
 pool.connect((err) => {
